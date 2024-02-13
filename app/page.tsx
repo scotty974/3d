837b97a262ea67596d2478f2c5af662fcb3377bf
scotty974 <fabienethevepro@gmail.com>
2024-02-13 19:01:51 +0100

    add effect glitch onclick

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 "use client";
-import Image from "next/image";
+
 import { Canvas } from "@react-three/fiber";
 import Sculpture from "./components/Sculpture/Sculpture";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+import { useRouter } from "next/navigation";
 import { PerspectiveCamera } from "@react-three/drei";
 import {
   EffectComposer,
@@ -16,18 +17,27 @@ import Link from "next/link";
 import { motion } from "framer-motion";
 
 export default function Home() {
-  
+  const router = useRouter();
+  const [delay, setDelay] = useState({ delay: { min: 1, max: 1.5 } });
+
+  const handleNavigateWorks = () => {
+    setDelay({ delay: { min: 0, max: 0 } });
+    setTimeout(() => {
+      router.push("/Work");
+    }, 3000);
+  };
+
   return (
     <motion.main
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    exit={{ opacity: 0 }}
-    transition={{ duration: 0.5 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
       className="min-h-screen "
     >
       <div className="flex justify-center items-center absolute -z-10">
         <Canvas dpr={[1, 2]} shadows>
-          <ambientLight intensity={2} />
+          <ambientLight intensity={1.5} />
           <directionalLight
             color="#7f00ff"
             position={[0, 0, 10]}
@@ -36,11 +46,11 @@ export default function Home() {
           <PerspectiveCamera position={[0, -2.5, 0]}>
             <Suspense fallback={null}>
               <Sculpture />
-              <EffectComposer >
+              <EffectComposer>
                 <Glitch
-                  delay={[1.5, 3.5]}
+                  delay={[delay.delay.min, delay.delay.max]}
                   duration={[0.6, 1.0]}
-                  strength={[0.3, 0.8]}
+                  strength={[0.3, 1]}
                   ratio={0.85}
                   mode={GlitchMode.SPORADIC}
                   active
@@ -56,13 +66,13 @@ export default function Home() {
         <Header></Header>
         <div className="h-screen flex items-center ">
           <nav className="text-white flex justify-between w-full">
+            <span className="text-4xl hover:underline hover:cursor-pointer" onClick={handleNavigateWorks}>
+              WORKS
+            </span>
             <Link
-              href="/Work"
+              href="/About"
               className="text-4xl hover:underline hover:cursor-pointer"
             >
-              WORKS
-            </Link>
-            <Link href='/About' className="text-4xl hover:underline hover:cursor-pointer">
               ABOUT
             </Link>
           </nav>
